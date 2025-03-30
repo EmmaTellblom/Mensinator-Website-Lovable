d2340b174b693a8602fa172d121085ed214b4417
Emma <emma.tellblom@gmail.com>
2025-03-30 16:04:54 +0200

    fixed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["mensinator.app"], // Allow this host
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
